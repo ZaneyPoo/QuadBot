551290ae5544276c0c694ad827e4b4ff2fc0f28b
Zane <zanehargis@gmail.com>
2024-03-18 19:48:32 -0700

    ref: rm unneeded scripts
    Now that we're running in a container, we don't really have any use for
    the virtual environment as well as these other scripts.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,53 +1,18 @@
 #!/usr/bin/env bash 
 
-source ./utils.sh
+print_start () {
+    echo "[ ] $1"
+}
 
-VENV=$(pwd)/.venv
-ENV_FILE=$(pwd)/.env
-USER_RW_PERMS=600
+print_success () {
+    echo "[X] Done!"
+}
 
-VENV_ERROR="couldn't create venv."
-DEPEND_ERROR="couldn't install dependencies." 
+print_error () { 
+    echo "$(tput setaf 1)$(tput bold)Error:$(tput setaf 7) $1"
+    echo "Exiting... $(tput sgr0)"
+    exit 1
+}
 
-if [[ ! -d $VENV ]];
-then 
-    print_start "Setting up Python's virtual environment"
-    python3 -m venv "$VENV" || print_error "$VENV_ERROR"
-    print_success
-fi
-
-print_start "Installing dependencies..."
-source .venv/bin/activate
 pip install -r ./requirements.txt || print_error "$DEPEND_ERROR"
 print_success
-
-if [[ $(has_systemd) ]];
-then
-    if [[ ! -e $PWD/qbot.service ]];
-    then 
-        print_start "Creating systemd unit file..."
-        echo "$SERVICE" > qbot.service
-        print_success
-
-        print_start "Enabling service..."
-        systemctl --user --now enable qbot.service
-    fi
-fi
-
-if [[ ! -e $ENV_FILE ]];
-then 
-    print "Creating .env file..."
-    read -p "Enter Discord API token: " -r -e TOKEN
-    echo "DISCORD_TOKEN=$TOKEN" > "$ENV_FILE"
-    chmod "$USER_RW_PERMS" .env || print_error "$CHMOD_ERROR"
-    print_success
-
-elif [[ $(stat -c "%a" "$ENV_FILE") -ne $USER_RW_PERMS ]];
-then
-    echo " Warning: the file permissions for your .env file aren't strict enough."
-    echo " Current: $(stat -c "%A" "$ENV_FILE")"
-    echo "Expected: (-rw------)"
-    print_start "Setting proper permissions..."
-    chmod "$USER_RW_PERMS" .env || print_error "$CHMOD_ERROR"
-    print_success
-fi

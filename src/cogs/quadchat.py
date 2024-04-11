b537a01aff16d3bc5ed802e5916c7b1bb448c99d
Zane Hargis <144561696+ZaneyPoo@users.noreply.github.com>
2024-04-11 14:33:37 -0700

    Add ChatHook framework (#16)
    Closes #13 
    
    * fix: add -u flag to python to rm buffering on stdout & stderr
    
    * feat: add new inspection dev commands + switch to per-cog error handling
    
    * feat: add working implementation of react hooks
    
    After hours of debugging, I got the react hooks working now by using
    custom events. A major pain in the ass due to Discord.py heavily lacking
    on documentation for registering events.
    
    Next step is to implement commands for adding and removing hooks
    
    NOTE TO FUTURE ME:
    Discord.py implicitly adds "on_" to the start of whatever event you try
    to dispatch. This isn't anywhere in the docs so I had to literally look
    at the source to find this.
    
    * style(QuadChat): rm debug print statements
    
    * fix(QuadDev): fix reload_all command not functioning
    
    I had forgotten to adjust the path to the cogs after rearranging the
    project structure sometime in the past. I should consider a more dynamic
    mechanism to set that path at startup.
    
    Ref: #13
    
    * feat(QuadReact): make hooks polymorphic
    
    I created a standard interface for the Hooks and used dynamic dispatch
    to eliminate visible runtime typechecking for the response type.
    I hope to add arbitrary callbacks in the future as well.
    
    * style(QuadDev): change reload log to say reloading extension
    
    * ref(QuadReact): swap implementation to use multiple regex patterns
    
    * style(QuadChat): rename default hooks constant
    
    * feat(QuadReact): add EventHook scaffolding for triggering arbitrary func
    
    * feat(QuadReact): add EventHooks for ++/-- elo system
    
    * style(QuadReact): add comments + formatting
    
    * feat(QuadReact): prevent users from targeting bots with chathooks
    
    * feat(QuadReact): add show_hooks command for displaying active ChatHooks

--- src/cogs/quadchat.py
+++ src/cogs/quadchat.py
@@ -1,12 +1,17 @@
+import logging
 from collections.abc import Callable
-from enum import Enum
 from dataclasses import dataclass
-from typing import Any
+from typing import Literal, Optional, Union
+import typing
 import discord
+from discord.app_commands import describe
 from discord.ext import commands
 from quadbot import QuadBot
+import re
 
-ANTI_SNAKE_TEMPLATE = (
+discord.utils.setup_logging(level=logging.DEBUG)
+
+_ANTI_SNAKE_EMBED_TEMPLATE = (
 """
 **Before**: 
 {before}
@@ -16,28 +21,225 @@ ANTI_SNAKE_TEMPLATE = (
 """.strip())
 
 
-class HookType(Enum):
-    REACTION = "reaction"
-    REPLY = "reply"
-    CALLBACK = "callback"
-
-
-@dataclass
-class ReactHook:
-    """
-    Data for reacting to messages in a customized way depending on various specified
-    parameters. 
-
-    :attr type_: The category 
-    :attr trigger: The necessary pattern to trigger the hook
-    :attr action: The action to take if the trigger is found in a message
-    :attr modifier: Additional parameters that must be true in order for the hook to trigger
-    """
-    type_: HookType
-    trigger: str
-    action: discord.Emoji | str | Callable
-    modifier: Callable[[Any], bool] | None = None 
+@dataclass(kw_only=True)
+class ChatHook:
+    pattern: str 
+    target: discord.Member | discord.User | Literal["global"] = "global"
+    modifier: Callable[..., bool] = lambda: True 
     enabled: bool = True
+
+    async def trigger(self, msg: discord.Message) -> None:
+        ...
+
+
+    def embed_field(self, embed: discord.Embed) -> discord.Embed:
+        return embed.add_field(
+            name=f"{str(self)}",
+            value='\n'.join([f"**{key}:** {value}" for key, value in vars(self).items()]),
+            inline=False
+        )
+
+
+# TODO: Can I fix all this code duplication?
+@dataclass(kw_only=True)
+class ReplyHook(ChatHook):
+    response: str
+
+    async def trigger(self, msg: discord.Message) -> None:
+        await msg.reply(self.response)
+
+
+    def __str__(self) -> str:
+        target = "anyone" if self.target == "global" else self.target.mention
+        return f"Reply with \"{self.response}\" if I see the pattern \"{self.pattern}\" from {target}"
+
+
+@dataclass(kw_only=True)
+class ReactHook(ChatHook):
+    response: discord.Emoji | str
+
+    async def trigger(self, msg: discord.Message) -> None:
+        await msg.add_reaction(self.response)
+
+
+    def __str__(self) -> str:
+        target = "anyone" if self.target == "global" else self.target.mention
+        return f"React with \"{self.response}\" if I see the pattern \"{self.pattern}\" from {target}"
+
+
+@dataclass(kw_only=True)
+class EventHook(ChatHook):
+    event_name: str
+    bot: commands.Bot
+
+    async def trigger(self, msg: discord.Message) -> None:
+        self.bot.dispatch(self.event_name, msg)
+
+
+    def __str__(self) -> str:
+        target = "anyone" if self.target == "global" else self.target.mention
+        return f"Trigger event \"{self.event_name}\" if I see the pattern \"{self.pattern}\" from {target}"
+
+
+class AddCHFlags(commands.FlagConverter):
+    """
+    blah
+    """
+    type_: Literal["reply", "reaction"] = (
+        commands.flag(name="type", 
+                      description="The type of ChatHook to add [reply | reaction]")
+    )
+    pattern: str = (
+        commands.flag(description="The pattern to search for.")
+    )
+    response: Union[discord.Emoji, str] = (
+        commands.flag(description="The sentence or emoji that QuadBot should react with")
+    )
+    target: Union[discord.Member, discord.User, Literal["global"]] = (
+        commands.flag(description="The user to match for or \"global\" to match everyone")
+    )
+    use_regex: bool = (
+        commands.flag(name="regex", 
+                      description="Whether or not the pattern uses regular expressions",
+                      default=False)
+    )
+    
+
+class QuadReact(commands.Cog):
+    def __init__(self, bot: QuadBot) -> None:
+        self.bot = bot
+        self._init_chathooks()
+
+
+    def _init_chathooks(self) -> None:
+        default_hooks = [
+            ReplyHook(
+                pattern="balls", 
+                response="balls"
+            ),
+            ReactHook(
+                pattern="skull", 
+                response=chr(0x1f480)
+            ),
+            EventHook(
+                pattern=r"\w+\+\+|<@!?\d+> \+\+", # Matches for a name or mention followed by ++
+                event_name="elo_increment", 
+                bot=self.bot
+            ),
+            EventHook(
+                pattern=r"\w+\-\-|<@!?\d+> \-\-", # Matches for a name or mention followed by --
+                event_name="elo_decrement", 
+                bot=self.bot
+            ),
+        ]
+        self.chathooks: dict[str, ChatHook] = {chathook.pattern: chathook for chathook in default_hooks}
+        self.compiled_patterns: list[re.Pattern] = [re.compile(key) for key in self.chathooks.keys()]
+
+
+    def add_chathook_pattern(self, chathook: ChatHook) -> None:
+        self.compiled_patterns.append(re.compile(chathook.pattern))
+        self.chathooks.update({chathook.pattern: chathook})
+            
+
+    # FIXME: I don't know why but the flag converter doesn't display the descriptions 
+    #        inside the help message for the command
+    @commands.command()
+    async def add_hook(self, ctx: commands.Context, *, flags: AddCHFlags) -> None:
+        print(f"Flags: {flags}")
+        if not flags.use_regex:
+            flags.pattern = re.escape(flags.pattern)
+
+        try: 
+            re.compile(flags.pattern)
+        except re.error as err:
+            await ctx.reply(f"Invalid regex: {err}")
+            return
+
+        if flags.target != "global" and flags.target.bot:
+            await ctx.reply(f"Cannot use a bot as the target for a ChatHook")
+            return
+
+        match flags.type_:
+            case "reply":
+                flags.response = typing.cast(str, flags.response)
+                chathook = ReplyHook(pattern=flags.pattern, 
+                                     response=flags.response,
+                                     target=flags.target)
+
+            case "reaction":
+                flags.response = typing.cast(discord.Emoji, flags.response)
+                chathook = ReactHook(pattern=flags.pattern,
+                                     response=flags.response,
+                                     target=flags.target)
+
+        self.add_chathook_pattern(chathook)
+        await ctx.reply("ChatHook added successfully")
+
+
+    # TODO: add command specific error handling
+    @add_hook.error
+    async def add_hook_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+        print(error)
+
+
+    @commands.command()
+    async def show_hooks(self, 
+                         ctx: commands.Context,
+                         verbose: Optional[bool] = False) -> None:
+        """
+        Show the currently active ChatHooks
+        """
+        embed = discord.Embed(
+            title="Current ChatHooks"
+        )
+        if verbose:
+            for hook in self.chathooks.values():
+                embed = hook.embed_field(embed)
+        else:
+            # TODO: Fix this ridiculous one liner 
+            embed.description = '\n'.join([f"{i}. {str(hook)}" for i, hook in enumerate(self.chathooks.values(), start=1)])
+        await ctx.reply(embed=embed)
+
+
+    @commands.Cog.listener()
+    async def on_chathook_trigger(self, msg: discord.Message, chathook: ChatHook) -> None:
+        if not chathook.enabled:
+            print(f"ChatHook {chathook} is not enabled!")
+            return 
+
+        if chathook.target != "global" and str(msg.author) != str(chathook.target):
+            print(f"Message author doesn't match ChatHook target: {chathook.target}")
+            return 
+
+        if not chathook.modifier():
+            print(f"ChatHook {chathook} failed modifier check!")
+            return 
+
+        await chathook.trigger(msg)
+
+
+    @commands.Cog.listener()
+    async def on_message(self, msg: discord.Message) -> None:
+        if msg.author.bot: # don't search if user is a bot
+            return
+
+        prefix = tuple(await self.bot.get_prefix(msg))
+        if msg.content.startswith(prefix): # don't search if this is a command
+            return
+
+        print(f"Scanning for patterns: {self.compiled_patterns}")
+        for pattern in self.compiled_patterns:
+            match = pattern.match(msg.content)
+            
+            if match:
+                print(f"Dispatching ChatHook for {match}")
+                self.bot.dispatch("chathook_trigger", msg, self.chathooks[match.re.pattern])
+
+
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        print("Loaded QuadReact cog!")
+
 
 
 class QuadChat(commands.Cog):
@@ -45,40 +247,6 @@ class QuadChat(commands.Cog):
     def __init__(self, bot: QuadBot) -> None:
         self.bot = bot
         self.edited_msg: dict[str, str] | None = None
-
-
-    # @commands.command()
-    # async def get_react_hook(self, 
-    #               ctx: commands.Context,
-    #               user: str) -> None:
-    #     try:
-    #         hook = self.bot.chatters[user.lower()]["react_hooks"]
-    #         await ctx.reply(f"```\nHook name:\n{user}\nValue:{hook}\n```")
-    #     except KeyError:
-    #         await ctx.reply(f"Unknown user: {user}")
-
-
-    # @commands.command()
-    # async def get_elo(self, 
-    #                   ctx: commands.Context,
-    #                   user: str) -> None:
-    #     try:
-    #         elo = self.bot.chatters[user.lower()]["elo"]
-    #         await ctx.reply(f"{user}'s ELO: {elo}")
-    #     except KeyError:
-    #         await ctx.reply(f"Unknown user: {user}")
-
-
-    #@commands.command()
-    #async def show_all(self, ctx: commands.Context) -> None:
-    #    await ctx.reply(f"```\n{self.bot.chatters}\n```")
-
-
-    # @commands.command()
-    # async def show_aliases(self, 
-    #                        ctx:commands.Context,
-    #                        user: str | None = None) -> None:
-    #     await ctx.reply(f"```\n{self.bot.aliases}\n```")
 
 
     @commands.command()
@@ -105,47 +273,32 @@ class QuadChat(commands.Cog):
         if self.edited_msg is None:
             await ctx.reply("No recently edited messages :sob:")
             return
-        
+
         embed = discord.Embed(
             title=":x: :snake: Antisnake! :x: :snake:",
-            description=ANTI_SNAKE_TEMPLATE.format(**self.edited_msg)
+            description=_ANTI_SNAKE_EMBED_TEMPLATE.format(**self.edited_msg)
         )
         await ctx.reply(embed=embed)
 
-        
-    # # commands.command(name="++")
-    # async def plusplus(self, 
-    #                    ctx: commands.Context, 
-    #                    member: discord.User | discord.Member) -> None:
-    #     chatter = self.bot.chatters[self.bot.get_real_name(member.name)]
-    #     chatter["elo"] += 1
-    #     await ctx.reply(f"{member.name} now has an ELO of {chatter['elo']}!")
+
+    # TODO: Hook this up to ELO system once DB is hooked up
+    @commands.Cog.listener()
+    async def on_elo_increment(self, msg: discord.Message) -> None:
+        # Is there a sane way for me to pass in the name of the target without rescanning?
+        print("++")
 
 
-    # # @commands.command(hidden=True, disabled=True)
-    # async def minusminus(self, 
-    #                      ctx: commands.Context, 
-    #                      member: discord.User | discord.Member) -> None:
-    #     chatter = self.bot.chatters[self.bot.get_real_name(member.name)]
-    #     chatter["elo"] -= 1
-    #     await ctx.reply(f"{member.name} now has an ELO of {chatter['elo']}!")
+    # TODO: Hook this up to ELO system once DB is hooked up
+    @commands.Cog.listener()
+    async def on_elo_decrement(self, msg: discord.Message) -> None:
+        # Is there a sane way for me to pass in the name of the target without rescanning?
+        print("--")
 
 
     @commands.Cog.listener()
-    async def on_message(self, msg: discord.Message) -> None:
-        if msg.author.bot: # gtfo if user is a bot
+    async def on_message(self, ctx: commands.Context) -> None:
+        if ctx.author.bot:
             return
-
-        # if (parts := msg.content.partition("++"))[1] == "++":
-        #     print(f"{parts[0]} plusplus")
-        #     ctx = await self.bot.get_context(msg)
-        #     await self.plusplus(ctx, msg.author)
-
-        # elif (parts := msg.content.partition("--"))[1] == "--":
-        #     print(f"{parts[0]} minusminus")
-        #     ctx = await self.bot.get_context(msg)
-        #     await self.minusminus(ctx, msg.author)
-
 
 
     @commands.Cog.listener()
@@ -158,9 +311,19 @@ class QuadChat(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, err: commands.CommandError) -> None:
         if isinstance(err, commands.CommandNotFound):
-            print("Unknown command.")
+            print(f"{err}: Unknown command.")
             await ctx.reply(self.bot.get_error_msg())
+        else:
+            print(err)
+
+
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        print("Loaded QuadChat cog!")
 
 
 async def setup(bot: QuadBot) -> None:
-    await bot.add_cog(QuadChat(bot))
+    await bot.add_cog(QuadReact(bot), override=True)
+    await bot.add_cog(QuadChat(bot), override=True)
+
+
--- src/cogs/quaddev.py
+++ src/cogs/quaddev.py
@@ -1,7 +1,38 @@
 import os
+import discord
 from discord.ext import commands
 
 # TODO: Setup logger
+discord.utils.setup_logging()
+
+COG_STATS_TEMPLATE = (
+"""
+**App Commands**:
+{app_cmds}
+**Commands**:
+{cmds}
+**Listeners**:
+{listeners}
+""".strip())
+
+def format_cog_list(bot: commands.Bot) -> discord.Embed:
+    cogs = '\n'.join([cog for cog in bot.cogs.keys()])
+    return discord.Embed(
+        title="Currently loaded Cogs:",
+        description=cogs
+    )
+
+
+def format_cog_stats(cog: commands.Cog) -> discord.Embed:
+    stats = {
+        "app_cmds": '\n'.join([str(app_cmd) for app_cmd in cog.walk_app_commands()]),
+        "cmds": '\n'.join([str(cmd) for cmd in cog.walk_commands()]),
+        "listeners": '\n'.join([str(listener) for listener in cog.get_listeners()]),
+    }
+    return discord.Embed(
+        title=f"Cog: {cog.qualified_name}",
+        description=COG_STATS_TEMPLATE.format(**stats)
+    )
 
 
 class QuadDev(commands.Cog):
@@ -11,14 +42,16 @@ class QuadDev(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def reload_all(self, ctx: commands.Context) -> None:
+    async def reload_all(self, _: commands.Context) -> None:
         """
         [Admin only] Reload all cogs.
         """
-        for filename in os.listdir("./cogs"):
+        # TODO: This should set to some variable (maybe from the bot instance) 
+        #       instead of being hard-coded in. This has already caused issues once.
+        for filename in os.listdir("./src/cogs"):
             if filename.endswith(".py"):
-                print(f"Reloading cog: {filename}")
-                await self.bot.reload_extension("cogs.{filename[:-3]}")
+                print(f"Reloading extension: {filename}")
+                await self.bot.reload_extension(f"cogs.{filename[:-3]}")
 
 
     @commands.command()
@@ -34,10 +67,43 @@ class QuadDev(commands.Cog):
             print(f"Synced command {cmd}")
 
 
+    @commands.command()
+    @commands.is_owner()
+    async def inspect(self, 
+                      ctx: commands.Context, 
+                      cog_name: str) -> None:
+        """
+        [Admin only] Give a detailed look at a specific Cog
+        """
+        cog = self.bot.get_cog(cog_name)
+
+        if cog is None:
+            await ctx.reply(f"Unable to find cog: {cog_name}")
+            return
+
+        embed = format_cog_stats(cog)
+        await ctx.reply(embed=embed)
+
+
+    @commands.command()
+    @commands.is_owner()
+    async def show_cogs(self, ctx: commands.Context) -> None:
+        """
+        [Admin only] Return a list of the Bot's currently loaded cogs
+        """
+        embed = format_cog_list(self.bot)
+        await ctx.reply(embed=embed)
+
+
+    @commands.Cog.listener()
+    async def on_cog_command_error(self, ctx: commands.Context, err: commands.CommandError) -> None:
+        await ctx.reply(f"Error: {err}")
+
+
     @commands.Cog.listener()
     async def on_ready(self) -> None:
         print("Loaded QuadDev cog!")
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(QuadDev(bot))
+    await bot.add_cog(QuadDev(bot), override=True)
